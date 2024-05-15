22dff5ebbe0917a78f1fb9e5b7bb0f2fd25fb468
EvolutionZ <139359712+EvolutionZ19@users.noreply.github.com>
2024-05-15 16:24:48 +0200

    Modification of the form to use pure JavaScript without touching the HTML, +

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -1,9 +1,9 @@
 function editNav() {
   var x = document.getElementById("myTopnav");
   if (x.className === "topnav") {
-    x.className += " responsive";
+      x.className += " responsive";
   } else {
-    x.className = "topnav";
+      x.className = "topnav";
   }
 }
 
@@ -54,119 +54,142 @@ const locations = document.querySelectorAll('input[name="location"]');
 const termsCheckbox = document.getElementById('checkbox1');
 
 // Form submit event
-form.addEventListener('submit', function(e) {
-    e.preventDefault(); // Prevent the form from being submitted and the page from refreshing
-    let valid = true;
+form.addEventListener('submit', function (e) {
+  e.preventDefault(); // Prevent the form from being submitted and the page from refreshing
+  let valid = true;
 
-    // Validation for First Name
-    if (firstName.value.trim().length < 2) {
-        valid = false;
-        document.getElementById('first_name_error').textContent = "Veuillez entrer 2 caractères ou plus pour le champ du prénom.";
-        firstName.style.border = '2px solid red';
-    } else {
-        document.getElementById('first_name_error').textContent = "";
-        firstName.style.border = '2px solid green';
-    }
-    // Add input event listener to first name input
-    firstName.addEventListener('input', function() {
-    document.getElementById('first_name_error').textContent = ""; // Clear the error message
-    firstName.style.border = ''; // Clear the red border
+  // Validation for First Name
+  if (firstName.value.trim().length < 2) { 
+      valid = false;  // If the first name is less than 2 characters, the form is not valid
+      let firstNameError = firstName.parentNode.querySelector('.error_message'); // Check if an error message already exists
+      if (!firstNameError) { // If an error message does not exist, create one
+          firstNameError = document.createElement('div');
+          firstNameError.classList.add('error_message');
+          firstName.parentNode.appendChild(firstNameError); // Append the error message to the parent of the first name input
+      }
+      firstNameError.innerHTML = "Veuillez entrer 2 caractères ou plus pour le champ du prénom.";
+      firstName.style.border = '2px solid red';
+  } else { 
+      const firstNameError = firstName.parentNode.querySelector('.error_message'); // If the first name is valid, check if an error message exists
+      if (firstNameError) { // If an error message exists, remove it
+          firstNameError.remove();
+      }
+      firstName.style.border = '2px solid green';
+  }
+
+  // Validation for Last Name
+  if (lastName.value.trim().length < 2) {
+      valid = false;
+      let lastNameError = lastName.parentNode.querySelector('.error_message');
+      if (!lastNameError) {
+          lastNameError = document.createElement('div');
+          lastNameError.classList.add('error_message');
+          lastName.parentNode.appendChild(lastNameError);
+      }
+      lastNameError.innerHTML = "Veuillez entrer 2 caractères ou plus pour le champ du nom.";
+      lastName.style.border = '2px solid red';
+  } else {
+      const lastNameError = lastName.parentNode.querySelector('.error_message');
+      if (lastNameError) {
+          lastNameError.remove();
+      }
+      lastName.style.border = '2px solid green';
+  }
+
+  // Validation for Email
+  if (!email.value.includes('@') || !email.value.includes('.')) {
+      valid = false;
+      let emailError = email.parentNode.querySelector('.error_message');
+      if (!emailError) {
+          emailError = document.createElement('div');
+          emailError.classList.add('error_message');
+          email.parentNode.appendChild(emailError);
+      }
+      emailError.innerHTML = "Veuillez entrer une adresse e-mail valide.";
+      email.style.border = '2px solid red';
+  } else {
+      const emailError = email.parentNode.querySelector('.error_message');
+      if (emailError) {
+          emailError.remove();
+      }
+      email.style.border = '2px solid green';
+  }
+
+  // Validation for Quantity
+  if (!(/^\d{1}$/).test(quantity.value.trim())) {
+      valid = false;
+      let quantityError = quantity.parentNode.querySelector('.error_message');
+      if (!quantityError) {
+          quantityError = document.createElement('div');
+          quantityError.classList.add('error_message');
+          quantity.parentNode.appendChild(quantityError);
+      }
+      quantityError.innerHTML = "Veuillez entrer un chiffre pour la quantité.";
+      quantity.style.border = '2px solid red';
+  } else {
+      const quantityError = quantity.parentNode.querySelector('.error_message');
+      if (quantityError) {
+          quantityError.remove();
+      }
+      quantity.style.border = '2px solid green';
+  }
+
+  // Validation for Birthdate
+  if (!birthdate.value) {
+      valid = false;
+      let birthdateError = birthdate.parentNode.querySelector('.error_message');
+      if (!birthdateError) {
+          birthdateError = document.createElement('div');
+          birthdateError.classList.add('error_message');
+          birthdate.parentNode.appendChild(birthdateError);
+      }
+      birthdateError.innerHTML = "Veuillez sélectionner une date de naissance valide.";
+      birthdate.style.border = '2px solid red';
+  } else {
+      const birthdateError = birthdate.parentNode.querySelector('.error_message');
+      if (birthdateError) {
+          birthdateError.remove();
+      }
+      birthdate.style.border = '2px solid green';
+  }
+
+  // Validation for Tournament Location
+  let locationChecked = false;
+  locations.forEach(function (location) {
+      if (location.checked) {
+          locationChecked = true;
+      }
   });
+  if (!locationChecked) {
+      valid = false;
+      let locationError = document.querySelector('.formData:last-child .error_message');
+      if (!locationError) {
+          locationError = document.createElement('div');
+          locationError.classList.add('error_message');
+          document.querySelector('.formData:last-child').appendChild(locationError);
+      }
+      locationError.innerHTML = "Veuillez sélectionner une option pour le tournoi.";
+  } else {
+      const locationError = document.querySelector('.formData:last-child .error_message');
+      if (locationError) {
+          locationError.remove();
+      }
+  }
 
-    // Validation for Last Name
-    if (lastName.value.trim().length < 2) {
-        valid = false;
-        document.getElementById('last_name_error').textContent = "Veuillez entrer 2 caractères ou plus pour le champ du nom.";
-        lastName.style.border = '2px solid red';
-    } else {
-        document.getElementById('last_name_error').textContent = "";
-        lastName.style.border = '2px solid green';
-    }
-    // Add input event listener to last name input
-     lastName.addEventListener('input', function() {
-      document.getElementById('last_name_error').textContent = ""; // Clear the error message
-      lastName.style.border = ''; // Clear the red border
-    });
-
-    // Validation for Email
-    if (!email.value.includes('@') || !email.value.includes('.')) {
-        valid = false;
-        document.getElementById('email_error').textContent = "Veuillez entrer une adresse e-mail valide.";
-        email.style.border = '2px solid red';
-    } else {
-        document.getElementById('email_error').textContent = "";
-        email.style.border = '2px solid green';
-    }
-    // Add input event listener to email input
-    email.addEventListener('input', function() {
-      document.getElementById('email_error').textContent = ""; // Clear the error message
-      email.style.border = ''; // Clear the red border
-    });
-
-    // Validation for Quantity
-    if (quantity.value.trim() === '' || isNaN(quantity.value)) {
-        valid = false;
-        document.getElementById('quantity_error').textContent = "Veuillez entrer un nombre valide pour la quantité.";
-        quantity.style.border = '2px solid red';
-    } else {
-        document.getElementById('quantity_error').textContent = "";
-        quantity.style.border = '2px solid green';
-    }
-
-    // Add input event listener to quantity input
-    quantity.addEventListener('input', function() {
-      document.getElementById('quantity_error').textContent = ""; // Clear the error message
-      quantity.style.border = ''; // Clear the red border
-    });
-
-    // Validation for Birthdate
-    if (!birthdate.value) {
-        valid = false;
-        document.getElementById('birthday_error').textContent = "Veuillez sélectionner une date de naissance valide.";
-        birthdate.style.border = '2px solid red';
-    } else {
-        document.getElementById('birthday_error').textContent = "";
-        birthdate.style.border = '2px solid green';
-    }
-    // Add input event listener to birthdate input
-    birthdate.addEventListener('input', function() {
-      document.getElementById('birthday_error').textContent = ""; // Clear the error message
-      birthdate.style.border = ''; // Clear the red border
-    });
-
-    // Validation for Tournament Location
-    let locationChecked = false;
-    locations.forEach(function(location) {
-        if (location.checked) {
-            locationChecked = true;
-        }
-    });
-    if (!locationChecked) {
-        valid = false;
-        document.getElementById('location_error').textContent = "Veuillez sélectionner une option pour le tournoi.";
-    } else {
-        document.getElementById('location_error').textContent = "";
-    }
-
-    // Add input event listener to location input
-    locations.forEach(function(location) {
-        location.addEventListener('input', function() {
-            document.getElementById('location_error').textContent = ""; // Clear the error message
-        });
-    });
-
-    // Validation for Checkbox Terms and Conditions
-    if (!termsCheckbox.checked) {
-        valid = false;
-        document.getElementById('terms_checkbox_error').textContent = "Veuillez accepter les conditions d'utilisation.";
-    } else {
-        document.getElementById('terms_checkbox_error').textContent = "";
-    }
-
-
-    // Submit the form if valid
-    if (valid) {
-        form.submit();
-    }
+  // Validation for Checkbox Terms and Conditions
+  if (!termsCheckbox.checked) {
+      valid = false;
+      let termsCheckboxError = termsCheckbox.parentNode.querySelector('.error_message');
+      if (!termsCheckboxError) {
+          termsCheckboxError = document.createElement('div');
+          termsCheckboxError.classList.add('error_message');
+          termsCheckbox.parentNode.appendChild(termsCheckboxError);
+      }
+      termsCheckboxError.innerHTML = "Veuillez accepter les conditions d'utilisation.";
+  } else {
+      const termsCheckboxError = termsCheckbox.parentNode.querySelector('.error_message');
+      if (termsCheckboxError) {
+          termsCheckboxError.remove();
+      }
+  }
 });
-
